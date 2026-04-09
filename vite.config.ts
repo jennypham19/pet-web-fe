f9ed2e92c834a8f0f08436fe9d610efab7fbd6be
anhpn <ngocanh.pham19042001@gmail.com>
2026-04-09 17:38:35 +0700

    pet-fe-1

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pet/", // ✅ QUAN TRỌNG
+  base: "/petz/", // ✅ QUAN TRỌNG
   plugins: [
     react(),
     tsconfigPaths(),
